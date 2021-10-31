3e3e1535674f7bec0ce3d6391556305227844338
Cornelia Herman <corneliabherman@gmail.com>
2021-10-31 16:50:05 -0400

    starting to create hmtl page base on responses

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const manager = require("./lib/Manager");
 const engineer = require("./lib/Engineer");
 const intern = require("./lib/Intern");
 const questions = require("./lib/questions");
+const page = require("./lib/createPage");
 
 const employeeArray = [];
+employeeInfo(null, employeeArray);
 
-employeeInfo(employeeArray);
+
 
 //Prompt
 ////spread array into new array - ...common questions, manager questions
